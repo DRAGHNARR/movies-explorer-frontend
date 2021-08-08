70939af3f88d97b5ba108a6104487c2e83a0b5de
Denis Kochnev <draghnarrok@gmail.ru>
2021-08-09 02:55:46 +0300

    second

--- ourtube/src/components/Head/Head.jsx
+++ ourtube/src/components/Head/Head.jsx
@@ -16,7 +16,7 @@ function Head({context}) {
         <nav className="head__nav-box">
         <div className="head__close" onClick={() => setIsOpen(false)}></div>
         <ul className="head__nav-list">
-          <li className="head__nav-item"><NavLink className="head__nav-link" to={context.main.to}>{context.main.title}</NavLink></li>
+          <li className="head__nav-item head__nav-item_additional"><NavLink className="head__nav-link" to={context.main.to}>{context.main.title}</NavLink></li>
           <li className="head__nav-item"><NavLink className="head__nav-link" to={context.movies.to}>{context.movies.title}</NavLink></li>
           <li className="head__nav-item"><NavLink className="head__nav-link" to={context.savedMovies.to}>{context.savedMovies.title}</NavLink></li>
           <li className="head__nav-item"><NavLink className="head__nav-link head__nav-account" to={context.profile.to}><img className="head__nav-account-pic" src={accountPic} alt="Иконка профиля"/><p className="head__nav-account-text">{context.profile.title}</p></NavLink></li>
--- ourtube/src/components/MoviesCard/MoviesCard.jsx
+++ ourtube/src/components/MoviesCard/MoviesCard.jsx
@@ -1,12 +1,19 @@
 import './MoviesCard.css';
+import React, { useState } from 'react';
 
 function MoviesCard({movie}) {
+  const [isSaved, setIsSaved] = useState(movie.isSaved);
+
   return (
     <figure className="movie">
       <img className="movie__pic" src={movie.link} alt={movie.title}/>
-      <figcaption className="movie__title">{movie.title}</figcaption>
-      <div className="movie__divider"></div>
-      <p className="movie__duration">{movie.duration}</p>
+      <div className="movie__description">
+        <div className="movie__description-line">
+          <figcaption className="movie__title">{movie.title}</figcaption>
+          <button className={isSaved ? "movie__saved" : "movie__unsaved"} onClick={() => setIsSaved(!isSaved)}></button>
+        </div>
+        <p className="movie__duration">{movie.duration}</p>
+      </div>
     </figure>
   )
 }
